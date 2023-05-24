d25c71c72eb2f342be31485ed23d75b593e51d02
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2023-05-25 01:36:36 +0530

    Create `RequestLoggerInterceptor`

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { EntitiesModule } from '../entities/entities.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { RequestLoggerInterceptor } from 'src/interceptors/request-logger.interceptor';
 
 @Module({
   imports: [EntitiesModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: RequestLoggerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
