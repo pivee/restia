2311b7409d64af18114db33067f2441a12da807e
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2023-05-24 20:20:50 +0530

    Add logs in `bootstrap()`

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000, () => {
+    Logger.log(
+      `Server is running on port ${process.env.PORT ?? 3000}`,
+      'NestApplication',
+    );
+  });
 }
 bootstrap();
