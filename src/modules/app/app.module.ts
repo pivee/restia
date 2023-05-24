02d7affd10014cf35ee5004ea2be8403d8c30ff2
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2023-05-24 23:18:09 +0530

    Sort imports

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
+import { EntitiesModule } from '../entities/entities.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EntitiesModule } from '../entities/entities.module';
 
 @Module({
   imports: [EntitiesModule],
