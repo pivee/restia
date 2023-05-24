c125c9167ab1fc23f5657b4f3c62c12677f41f7f
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2023-05-24 20:33:46 +0530

    Enable `swagger.json` generation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { Logger } from '@nestjs/common';
+import * as cp from 'child_process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  if (process.env.NODE_ENV !== 'production') {
+    cp.execSync('npx nestia swagger');
+  }
+
   await app.listen(process.env.PORT ?? 3000, () => {
     Logger.log(
       `Server is running on port ${process.env.PORT ?? 3000}`,
